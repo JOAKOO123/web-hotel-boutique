558aa71fbde891708d69ad892c8b16ee1177f5ad
joakoo123 <beltranlive9@gmail.com>
2026-09-14 17:10:19 -0300

    chore: initial Angular project setup

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  cognitoUserPoolId: 'YOUR_COGNITO_USER_POOL_ID',
+  cognitoClientId: 'YOUR_COGNITO_CLIENT_ID',
+  cognitoDomain: 'YOUR_COGNITO_DOMAIN',
+  apiGatewayUrl: 'https://your-api-gateway-url.amazonaws.com'
+};
