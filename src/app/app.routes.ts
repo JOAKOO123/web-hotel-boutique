03a08f7024551c2ce117293cbaa74863fa22fb77
joakoo123 <beltranlive9@gmail.com>
2026-09-14 22:39:02 -0300

    feat: add shared header and main layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'reservations',
+		loadComponent: () =>
+			import('./features/reservations/reservations-list/reservations-list')
+				.then(m => m.ReservationsList)
+	},
+	{ path: '', redirectTo: 'reservations', pathMatch: 'full' }
+];
---
+++ src/app/features/reservations/reservations-list/reservations-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservations-list',
+  imports: [],
+  templateUrl: './reservations-list.html',
+  styleUrl: './reservations-list.scss',
+})
+export class ReservationsList {}
