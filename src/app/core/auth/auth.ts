c16506796c6081bb6a3a741eda4ab41e4ff8cbc0
lsn33 <lucassoto3333@gmail.com>
2026-09-22 01:52:24 -0300

    refactor: corregir URLs, scopes y endpoint de cierre de sesión en AWS Cognito

--- src/app/core/auth/auth.ts
+++ src/app/core/auth/auth.ts
@@ -10,7 +10,7 @@ export class Auth {
     redirect_uri: `${window.location.origin}/auth/callback`,
     post_logout_redirect_uri: `${window.location.origin}/`,
     response_type: 'code',
-    scope: 'openid email profile',
+    scope: 'openid email phone',
     automaticSilentRenew: false,
     loadUserInfo: true
   });
